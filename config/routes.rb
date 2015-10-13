2ae1c72431c26c0e32707a81c21bca2940958508
Petko Bordjukov <bordjukov@gmail.com>
2015-10-13 16:15:16 +0300

    Hide the results

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :talk_preferences, only: [:index, :show, :create, :update]
   root to: 'home#index'
-  get 'ratings' => 'home#ratings'
-  get 'export' => 'home#export'
+  # get 'ratings' => 'home#ratings'
+  # get 'export' => 'home#export'
 end
