6e3f6cca3504881610291e19bc81c7781b84cb58
Petko Bordjukov <bordjukov@gmail.com>
2016-10-01 02:09:59 +0300

    Update the Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.6.1'
 
 set :application, 'gauge'
 set :repo_url, 'https://github.com/OpenFest/gauge.git'
@@ -13,20 +13,21 @@ set :deploy_to, '/home/barf/gauge'
 # Default value for :scm is :git
 # set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# Default value for :log_level is :debug
-set :log_level, :info
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3')
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'db/production.sqlite3'
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
