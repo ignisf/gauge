eff79dab6391a7c6284b27429688857309526c07
Petko Bordjukov <bordjukov@gmail.com>
2016-10-06 23:09:53 +0300

    Fix syntax errors in the conflict coefficient

--- app/models/conflict_coefficient.rb
+++ app/models/conflict_coefficient.rb
@@ -19,11 +19,10 @@ class ConflictCoefficient
   end
 
   def conflicts
-    talk_preferences.joins('INNER JOIN "selected_talks" AS left
+    talk_preferences.joins('INNER JOIN "selected_talks" AS "left"
                             ON "left"."talk_preference_id" = "talk_preferences"."unique_id"
-                            INNER JOIN "selected_talks" AS right
-                            ON "right"."talk_preference_id" = "talk_preferences"."unique_id"')
-    .where(left: {talk_id: @left}, right: {talk_id: @right}).count
+                            INNER JOIN "selected_talks" AS "right"
+                            ON "right"."talk_preference_id" = "talk_preferences"."unique_id"').where(left: {talk_id: @left}, right: {talk_id: @right}).count
   end
 
   def total_votes
