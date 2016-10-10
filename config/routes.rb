e930e51f481260c009b38850d239c04df485a6f0
Petko Bordjukov <bordjukov@gmail.com>
2016-10-10 20:21:04 +0300

    Remove API from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   resources :talk_preferences, only: [:index, :show, :create, :update]
   root to: 'home#index'
-  get 'ratings' => 'home#ratings'
-  get 'export' => 'home#export'
-  get 'summary' => 'home#summary'
-  resources :conflicts do
-    collection do
-      get 'pivot'
-    end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
