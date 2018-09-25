772d1a5aa08b0525e1a7464a6340099e18d94b95
Petko Bordjukov <bordjukov@gmail.com>
2018-09-25 13:18:08 +0300

    Upgrade to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,14 @@ module Gauge
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:bg, :en]
     config.i18n.default_locale = :bg
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
