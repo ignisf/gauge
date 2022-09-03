e86dc6131b74979256c234539b9833d7ec4fd2f5
Petko Bordjukov <bordjukov@gmail.com>
2022-09-03 12:06:01 +0300

    Update for OF 2022

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,7 +1,7 @@
 class Talk < ActiveResource::Base
   has_many :selections, class_name: 'SelectedTalk'
 
-  self.site = "https://cfp.openfest.org/api/conferences/7"
+  self.site = "https://cfp.openfest.org/api/conferences/9"
   self.element_name = "event"
 
   def self.ordered_by_id
